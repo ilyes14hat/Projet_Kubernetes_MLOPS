e396483b037ac5e7dcec9ae1586b412a249b19a2
anismahmahi <ja_mahmahi@esi.dz>
2023-12-08 23:10:44 +0100

    all services work

--- predict/app.py
+++ predict/app.py
@@ -2,40 +2,50 @@
 from flask import Flask, request, jsonify
 import requests
 from PIL import Image
+import keras.utils as image
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
-
+import os
 app = Flask(__name__)
 
 # Load model
 model = load_model('projetIMN.h5')
+
 
 # Function to make a prediction using the model
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the image file from the request
     uploaded_file = request.files['image']
-    print("fffffffffffffffffffffffffffffffffffffff",request.files)
-
     img = Image.open(uploaded_file)
+    img.save('temprory.jpg')
+    preprocessed_data= []
+    
+    # Pass the image to preprocess_input microservice
+    with open('temprory.jpg', 'rb') as f:
+        preprocess_input_url = 'http://127.0.0.1:5001/preprocess_input'
+        r = requests.post(preprocess_input_url, files={'image': f})
+        preprocessed_data = np.array(r.json()['preprocessed_data'])
+    os.remove('temprory.jpg')
 
-    # # Pass the image to preprocess_input2 microservice
-    # preprocess_input2_url = 'http://127.0.0.0:5001/preprocess_input2'
-    # response = requests.post(preprocess_input2_url, json={'image': img.tolist()})
+    # preprocess_input_url = 'http://127.0.0.1:5001/preprocess_input'
+    # response = requests.post(preprocess_input_url, files={'image': uploaded_file})
     # preprocessed_data = np.array(response.json()['preprocessed_data'])
 
-    # # Make a prediction using the preprocessed data
-    # Array_IMG = preprocessed_data.reshape((1,) + preprocessed_data.shape)
-    # val_gen = ImageDataGenerator()
-    # input_data = val_gen.flow(Array_IMG, batch_size=1)
-    # prediction = model.predict(input_data)
-    # label = np.argmax(prediction)
 
-    # classes = {0: 'EOSINOPHIL', 1: 'LYMPHOCYTE', 2: 'MONOCYTE', 3: 'NEUTROPHIL'}
-    # result = {'prediction': classes[label]}
+    #################################   Works Excellent   ###########################################
+    # Load_IMG = img.resize((224,224))
+    # Array_IMG = image.img_to_array(Load_IMG)
+    # Array_IMG = Array_IMG.reshape((1,) + Array_IMG.shape)
+    # Array_IMG /= 127.5
+    # Array_IMG -= 1.
+    #################################################################################################
 
-    # return jsonify(result)
+    prediction = model.predict(preprocessed_data)
+    label = np.argmax(prediction)
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='127.0.0.1', port=5000)
+    classes = {0: 'EOSINOPHIL', 1: 'LYMPHOCYTE', 2: 'MONOCYTE', 3: 'NEUTROPHIL'}
+    result = {'prediction': classes[label]}
+
+    return jsonify(result)
--- ui/ui.py
+++ ui/ui.py
@@ -8,11 +8,8 @@ def main():
 
     # Upload image file
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-    # time.sleep(30)
-    # print('ffffffffffffffffff',uploaded_file.read())
-
+ 
     if uploaded_file is not None:
-        # print('not none = ',uploaded_file)
         image = Image.open(uploaded_file)
         
         st.image(image, caption='Uploaded Image', use_column_width=True)
@@ -20,16 +17,14 @@ def main():
         # Button to trigger classification
         if st.button('Classify'):
             # Convert image to bytes
-            # print('not none = ',uploaded_file)
             img_bytes = uploaded_file.getvalue()
-            #print("from ui =================", img_bytes[0:30])
 
             # Send the image to the predict microservice
-            #predict_microservice_url = 'http://predict-microservice:5000/predict'
             predict_microservice_url = 'http://127.0.0.1:5000/predict'
             files = {'image': img_bytes}
-            # print(files)
+            print(img_bytes)
             response = requests.post(predict_microservice_url, files=files)
+            print(response)
 
             # Display the prediction
             if response.status_code == 200:
